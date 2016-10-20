fd2d41b6dc9ddaca2cb98e8cdfae537902b4e174
Sirisha Nookala <sirishamnookala@fs.fed.us>
2016-10-20 15:37:17 -0400

    initial check in with travis ci

---
+++ TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApp
+{
+    
+    class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
